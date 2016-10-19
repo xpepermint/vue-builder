4197dd08f0c0487dd480611f53c22ff71f29ab7f
xpeper <xpepermint@gmail.com>
2016-10-19 23:55:28 +0200

    adding tests

---
+++ tests/assets/app/components/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    Hello World
+    <img src="../images/logo.png">
+  </div>
+</template>
+
+<style>
+body {
+  background: #c0c0c0;
+}
+</style>
---
+++ tests/index.js
@@ -0,0 +1,79 @@
+const test = require('ava');
+const webpack = require('webpack');
+const fs = require('fs');
+const {build} = require('vue-webpack');
+const {VueBuilder, VueRender} = require('../src');
+
+test('VueBuilder.prototype.build', async (t) => {
+  let config = build({
+    env: 'development',
+    mode: 'client',
+    manifest: true,
+    inputFilePath: './assets/app/client-entry.js',
+    outputFileName: 'bundle',
+    outputPath: '../dist/client',
+    publicPath: '/assets/',
+  });
+  let compiler = webpack(config);
+  let builder = new VueBuilder({compiler});
+  await builder.build();
+
+  t.is(fs.existsSync('../dist/client/images/logo.png'), true);
+  t.is(fs.existsSync('../dist/client/bundle.js'), true);
+  t.is(fs.existsSync('../dist/client/bundle.css'), true);
+  t.is(fs.existsSync('../dist/client/bundle.js.map'), true);
+  t.is(fs.existsSync('../dist/client/bundle.css.map'), true);
+});
+
+test('VueBuilder.prototype.render()', async (t) => {
+  let config = build({
+    env: 'production',
+    mode: 'server',
+    manifest: true,
+    inputFilePath: './assets/app/server-entry.js',
+    outputFileName: 'bundle',
+    outputPath: '../dist/server'
+  });
+  let compiler = webpack(config);
+  let builder = new VueBuilder({compiler});
+  let source = await builder.render();
+
+  t.is(source.indexOf('module.exports=f'), 0);
+});
+
+test('VueRender.prototype.renderToStream()', async (t) => {
+  let config = build({
+    env: 'production',
+    mode: 'server',
+    manifest: true,
+    inputFilePath: './assets/app/server-entry.js',
+    outputFileName: 'bundle',
+    outputPath: '../dist/server'
+  });
+  let compiler = webpack(config);
+  let builder = new VueBuilder({compiler});
+  let source = await builder.render();
+  let render = new VueRender({source});
+  let stream = render.renderToStream();
+
+  t.is(!!stream.pipe, true);
+});
+
+test('VueRender.prototype.renderToString()', async (t) => {
+  let config = build({
+    env: 'production',
+    mode: 'server',
+    manifest: true,
+    inputFilePath: './assets/app/server-entry.js',
+    outputFileName: 'bundle',
+    outputPath: '../dist/server'
+  });
+  let compiler = webpack(config);
+  let builder = new VueBuilder({compiler});
+  let source = await builder.render();
+  let render = new VueRender({source});
+  let html = await render.renderToString();
+
+  t.is(typeof html, 'string');
+  t.is(html.indexOf('<div id="app" server-rendered="true">'), 0);
+});
